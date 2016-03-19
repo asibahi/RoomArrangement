4b92fa4ce4fbb66b70eb5a3216d12e8f52339098
Abdurrahman S <a.sibahi@gmail.com>
2016-03-19 05:47:47 -0500

    Added a method to compare rooms using the Gaussian function

--- RoomArrangement/RoomGeneticElements.cs
+++ RoomArrangement/RoomGeneticElements.cs
@@ -77,19 +77,6 @@ namespace RoomArrangement
                         return (a || b);
                 }
 
-                private static void ReadRoom(int i,
-                                                out double recX,
-                                                out double recY,
-                                                out double cntX,
-                                                out double cntY)
-                {
-                        var r = Database.List[i];
-                        recX = r.Space.XDimension;
-                        recY = r.Space.YDimension;
-                        cntX = r.Center.X;
-                        cntY = r.Center.Y;
-                }
-
                 // I am still not sure what I should have this method return
                 // It should compare whether two rooms intersect and if they are related, how far they are.
                 private static double CompareRooms(int i, int j)
@@ -106,22 +93,66 @@ namespace RoomArrangement
                                 ReadRoom(i, out xRec1, out yRec1, out xCnt1, out yCnt1);
                                 ReadRoom(j, out xRec2, out yRec2, out xCnt2, out yCnt2);
 
-                                double xDis = Abs(xCnt1 - xCnt2);
-                                double yDis = Abs(yCnt1 - yCnt2);
-                                double xSize = (xRec1 / 2) + (xRec2 / 2);
-                                double ySixe = (yRec1 / 2) + (yRec2 / 2);
+                                double xDim = Abs(xCnt1 - xCnt2) - ((xRec1 / 2) + (xRec2 / 2));
+                                double yDim = Abs(yCnt1 - yCnt2) - ((yRec1 / 2) + (yRec2 / 2));
 
-                                // Intersection logic
-                                if (xSize > xDis && ySixe > yDis)
+                                // Related Rooms logic
+                                bool roomsRelated = ri.AdjacentRooms.Contains(rj);
+
+                                if (roomsRelated)
                                 {
+                                        if ((xDim == 0 && yDim < 0) || (xDim < 0 && yDim == 0))
+                                                returnVal = 1;
+
+                                        else if (xDim == 0 && yDim == 0)
+                                                returnVal = GaussianFunc(1);
+
+                                        else if ((xDim > 0 && yDim < 0) || (xDim < 0 && yDim > 0))
+                                                returnVal = GaussianFunc(Max(xDim, yDim));
+
+                                        else
+                                                returnVal = GaussianFunc(xDim) * GaussianFunc(yDim);
 
                                 }
-
-                                if (ri.AdjacentRooms.Contains(rj))
+                                else
                                 {
+                                        // Intersection logic
+                                        if (xDim < 0 && yDim < 0)
+                                                returnVal = GaussianFunc(xDim) * GaussianFunc(yDim);
+
                                 }
                         }
                         return returnVal;
+                }
+
+                // sy's idea. makes the code "neater".
+                private static void ReadRoom(int i,
+                                             out double recX,
+                                             out double recY,
+                                             out double cntX,
+                                             out double cntY)
+                {
+                        var r = Database.List[i];
+                        recX = r.Space.XDimension;
+                        recY = r.Space.YDimension;
+                        cntX = r.Center.X;
+                        cntY = r.Center.Y;
+                }
+
+                // Implementing the Gaussian Function (bell curve) when
+                // a (peak)     = 1
+                // b (center)   = 0
+                // c (width)    = 5 because what the hell. It is the only thing that could/should be changed.
+                // Check https://en.wikipedia.org/wiki/Gaussian_function and
+                // http://www.wolframalpha.com/input/?i=f%5Cleft(x%5Cright)+%3D+e%5E%7B-+%7B+%5Cfrac%7B(x)%5E2+%7D%7B+2+*+1%5E2%7D+%7D+%7D
+                // for details
+                private static double GaussianFunc(double x)
+                {
+                        double a = 1;
+                        double b = 0;
+                        double c = 5;
+
+                        return (a * Pow(E, -(Pow((x - b), 2) / (2 * Pow(c, 2)))));
                 }
         }
 }
