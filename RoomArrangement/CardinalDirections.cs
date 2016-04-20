9b7736fcb3140998232c48a688992fefbcdfcecf
Abdurrahman S <a.sibahi@gmail.com>
2016-04-20 00:24:09 -0500

    File organization

---
+++ RoomArrangement/CardinalDirections.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace RoomArrangement
+{
+	[Flags]
+	public enum CardinalDirections
+	{
+		None = 0,
+		North = 1 << 0,
+		East = 1 << 1,
+		South = 1 << 2,
+		West = 1 << 3
+	}
+}
--- RoomArrangement/InputRooms.cs
+++ RoomArrangement/InputRooms.cs
@@ -2,16 +2,6 @@
 
 namespace RoomArrangement
 {
-	[Flags]
-	public enum CardinalDirections
-	{
-		None = 0,
-		North = 1 << 0,
-		East = 1 << 1,
-		South = 1 << 2,
-		West = 1 << 3
-	}
-
 	// For input purposes. Does not need to match the room types. IF I am ever
 	// opening this library I should find a better solution than matching a list
 	// of radio-buttons.
